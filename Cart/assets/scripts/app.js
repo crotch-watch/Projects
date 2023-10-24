f3d60ac09dfc32bd6d515ee96b523e91925672a6
crotch-watch <naps_08_bay@icloud.com>
2023-10-24 23:15:40 +0530

    feat: added cart & shop

--- Cart/assets/scripts/app.js
+++ Cart/assets/scripts/app.js
@@ -12,12 +12,37 @@ class Product {
   }
 }
 
-class ProductsListItem {
-  constructor(product) {
-    this.product = product;
+class Cart {
+  constructor() {
+    this.items = [0];
   }
 
-  render() {
+  addToCart = (price) => {
+    console.log(this);
+    this.items.push(price);
+    // Cart.items.push(price)
+  };
+
+  init() {
+    const cart = document.createElement("section");
+    cart.innerHTML = `
+        <h2>Total ${this.items.reduce((acc, curr) => acc + curr)}</h2>
+        <button>Order Now</button>
+      `;
+    cart.className = "cart";
+
+    return cart;
+  }
+}
+
+class ProductsListItem {
+  constructor(product, addToCart, self) {
+    this.product = product;
+    this.addToCart = addToCart;
+    this.self = self;
+  }
+
+  init() {
     const productItem = document.createElement("li");
     productItem.className = "product-item";
 
@@ -33,55 +58,72 @@ class ProductsListItem {
       </section>
     `;
 
+    const buyBtn = productItem.querySelector("button");
+    buyBtn.addEventListener("click", () =>
+      this.addToCart(this.product.price, this.self)
+    );
+
     return productItem;
   }
 }
 
 class ProductsList {
-  products = [
-    new Product(
-      "Heading",
-      "https://en.wikipedia.org/wiki/Cinematography#/media/File:Arri_Alexa_camera.jpg",
-      "Dummy Description",
-      19.99
-    ),
-    new Product(
-      "Heading",
-      "https://en.wikipedia.org/wiki/Cinematography#/media/File:Arri_Alexa_camera.jpg",
-      "Dummy Description",
-      29.99
-    ),
-    new Product(
-      "Heading",
-      "https://en.wikipedia.org/wiki/Cinematography#/media/File:Arri_Alexa_camera.jpg",
-      "Dummy Description",
-      39.99
-    ),
-    new Product(
-      "Heading",
-      "https://en.wikipedia.org/wiki/Cinematography#/media/File:Arri_Alexa_camera.jpg",
-      "Dummy Description",
-      49.99
-    ),
-  ];
+  constructor(addToCart) {
+    this.addToCart = addToCart;
 
-  constructor(productsList) {
-    this.productsList = productsList;
+    this.products = [
+      new Product(
+        "Heading",
+        "https://en.wikipedia.org/wiki/Cinematography#/media/File:Arri_Alexa_camera.jpg",
+        "Dummy Description",
+        19.99
+      ),
+      new Product(
+        "Heading",
+        "https://en.wikipedia.org/wiki/Cinematography#/media/File:Arri_Alexa_camera.jpg",
+        "Dummy Description",
+        29.99
+      ),
+      new Product(
+        "Heading",
+        "https://en.wikipedia.org/wiki/Cinematography#/media/File:Arri_Alexa_camera.jpg",
+        "Dummy Description",
+        39.99
+      ),
+      new Product(
+        "Heading",
+        "https://en.wikipedia.org/wiki/Cinematography#/media/File:Arri_Alexa_camera.jpg",
+        "Dummy Description",
+        49.99
+      ),
+    ];
   }
 
-  render() {
-    const renderHook = document.getElementById("app");
+  init() {
     const productsList = document.createElement("ul");
     productsList.className = "product-list";
 
     this.products.forEach((product) => {
-      const productItem = new ProductsListItem(product)
-      productsList.append(productItem.render());
+      const productListItem = new ProductsListItem(product, this.addToCart);
+      productsList.append(productListItem.init());
     });
 
-    renderHook.append(productsList);
+    return productsList;
   }
 }
 
-const productList1 = new ProductsList();
-productList1.render();
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
+
+    const cart = new Cart();
+    console.dir(cart);
+    renderHook.append(cart.init());
+
+    const productList = new ProductsList(cart.addToCart);
+    renderHook.append(productList.init());
+  }
+}
+
+const shop = new Shop();
+shop.render();
