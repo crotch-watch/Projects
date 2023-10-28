36a216b1b24eb63f6ab027ccf54d8f0a7c6d4c9b
crotch-watch <naps_08_bay@icloud.com>
2023-10-28 18:02:36 +0530

    feat: added map and popups

--- Mapty/script.js
+++ Mapty/script.js
@@ -10,3 +10,40 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function (data) {
+    const map = L.map('map').setView(
+      [data.coords.latitude, data.coords.latitude],
+      13
+    );
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([data.coords.latitude, data.coords.longitude], {
+      opacity: 0.85,
+    })
+      .addTo(map)
+      .bindPopup('Running')
+      .openPopup();
+
+    map.on('click', function (event) {
+      L.marker([event.latlng.lat, event.latlng.lng], {
+        opacity: 0.85,
+      })
+        .addTo(map)
+        .bindPopup('Running');
+
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      });
+    });
+  }, console.warn);
+}
