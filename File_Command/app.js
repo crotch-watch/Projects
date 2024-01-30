dc60ac84863e9a8873651e22bfb540437919f20b
crotch-watch <naps_08_bay@icloud.com>
2024-01-31 00:01:28 +0530

    feat: create file command

--- File_Command/app.js
+++ File_Command/app.js
@@ -1,9 +1,25 @@
 const fs = require("node:fs/promises");
 const { Buffer } = require("node:buffer");
 
+const CREATE = "create";
+
+const commands = {
+  [CREATE]: CREATE,
+};
+
 (async () => {
   const fileHandler = await fs.open("./command.txt", "r");
   const watcher = fs.watch("./command.txt");
+
+  const createFile = async (name) => {
+    try {
+      await fs.open(name, "r");
+    } catch (error) {
+      const fileHandler = await fs.open(name, "w");
+      console.log("created a file");
+      fileHandler.close();
+    }
+  };
 
   fileHandler.on("change", async () => {
     const file_data = await fileHandler.stat();
@@ -13,10 +29,19 @@ const { Buffer } = require("node:buffer");
     const file_buffer = Buffer.alloc(size);
     const { byteLength } = file_buffer;
 
-    const { buffer: file } = await fileHandler.read(file_buffer, offset, byteLength, position);
+    await fileHandler.read(file_buffer, offset, byteLength, position);
 
-    console.log(file);
-    console.log(file.toString("utf-8"));
+    const command_line = file_buffer.toString("utf-8");
+
+    if (command_line.includes(commands.create)) {
+      try {
+        const nameStarts = commands.create.length + 1;
+        const file_name = command_line.slice(nameStarts);
+        createFile(file_name);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   });
 
   for await (const event of watcher) {
