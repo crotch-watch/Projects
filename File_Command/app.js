64fe105d457d289ee356e780bf20ba022c7ca3f1
crotch-watch <naps_08_bay@icloud.com>
2024-01-31 00:00:34 +0530

    feat: event emiiter

--- File_Command/app.js
+++ File_Command/app.js
@@ -5,19 +5,21 @@ const { Buffer } = require("node:buffer");
   const fileHandler = await fs.open("./command.txt", "r");
   const watcher = fs.watch("./command.txt");
 
+  fileHandler.on("change", async () => {
+    const file_data = await fileHandler.stat();
+    const { size } = file_data;
+    const position = 0;
+    const offset = position;
+    const file_buffer = Buffer.alloc(size);
+    const { byteLength } = file_buffer;
+
+    const { buffer: file } = await fileHandler.read(file_buffer, offset, byteLength, position);
+
+    console.log(file);
+    console.log(file.toString("utf-8"));
+  });
+
   for await (const event of watcher) {
-    if (event.eventType === "change") {
-      const file_data = await fileHandler.stat();
-
-      const { size } = file_data;
-      const position = 0;
-      const offset = position;
-      const file_buffer = Buffer.alloc(size);
-      const { byteLength } = file_buffer;
-
-      const { buffer: file } = await fileHandler.read(file_buffer, offset, byteLength, position);
-      console.log(file);
-      console.log(file.toString("utf-8"));
-    }
+    if (event.eventType === "change") fileHandler.emit("change");
   }
 })();
