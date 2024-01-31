20f0da544f2f56b300c6e2b49699e97a4d26473b
crotch-watch <naps_08_bay@icloud.com>
2024-01-31 23:47:10 +0530

    feat: CRUD operations

--- File_Command/app.js
+++ File_Command/app.js
@@ -2,48 +2,76 @@ const fs = require("node:fs/promises");
 const { Buffer } = require("node:buffer");
 
 const CREATE = "create";
+const DELETE = "delete";
+const RENAME = "rename";
+const APPEND = "append";
 
-const commands = {
-  [CREATE]: CREATE,
+const createFile = async (name) => {
+  try {
+    const existingFileHandle = await fs.open(name, "r");
+    await existingFileHandle.close();
+  } catch (error) {
+    const fileHandler = await fs.open(name, "w");
+    console.log(`created ${name}`);
+    await fileHandler.close();
+  }
+};
+const deleteFile = async (name) => {
+  try {
+    await fs.unlink(name);
+    console.log("Deleted ", name);
+  } catch ({ code }) {
+    if (code === "ENOENT") console.log(`${name} doesn't exist`);
+    else console.log("code: ", code);
+  }
+};
+const renameFile = async (name, newName) => {
+  try {
+    await fs.rename(name, newName);
+    console.log(`${name} has been renmaed to ${newName}`);
+  } catch ({ code }) {
+    if (code === "ENOENT") console.log(`${name} doesn't exist`);
+    else console.log("code: ", code);
+  }
+};
+const appendFile = async (name, data) => {
+  let fileHandler;
+  try {
+    fileHandler = await fs.open(name, "a");
+    await fileHandler.write(data);
+    console.log("data has been appended to the file");
+  } catch ({ code }) {
+    if (code === "ENOENT") console.log(`${name} doesn't exist`);
+    else console.log("code :", code);
+  } finally {
+    await fileHandler?.close();
+  }
 };
 
 (async () => {
   const fileHandler = await fs.open("./command.txt", "r");
-  const watcher = fs.watch("./command.txt");
-
-  const createFile = async (name) => {
-    try {
-      await fs.open(name, "r");
-    } catch (error) {
-      const fileHandler = await fs.open(name, "w");
-      console.log("created a file");
-      fileHandler.close();
-    }
-  };
 
   fileHandler.on("change", async () => {
-    const file_data = await fileHandler.stat();
-    const { size } = file_data;
+    const size = (await fileHandler.stat()).size;
+    const buffer = Buffer.alloc(size);
+    const offset = 0;
+    const { byteLength: length } = buffer;
     const position = 0;
-    const offset = position;
-    const file_buffer = Buffer.alloc(size);
-    const { byteLength } = file_buffer;
 
-    await fileHandler.read(file_buffer, offset, byteLength, position);
+    await fileHandler.read(buffer, offset, length, position);
+    const command_line = buffer.toString("utf-8");
+    const [command, fileName] = command_line.split(" ", 2);
 
-    const command_line = file_buffer.toString("utf-8");
+    const initOptionIndex = command.length + 1 + fileName.length + 1;
+    const option = command_line.slice(initOptionIndex);
 
-    if (command_line.includes(commands.create)) {
-      try {
-        const nameStarts = commands.create.length + 1;
-        const file_name = command_line.slice(nameStarts);
-        createFile(file_name);
-      } catch (error) {
-        console.log(error);
-      }
-    }
+    if (command === CREATE) createFile(fileName);
+    if (command === DELETE) deleteFile(fileName);
+    if (command === RENAME) renameFile(fileName, option);
+    if (command === APPEND) appendFile(fileName, option);
   });
 
+  const watcher = fs.watch("./command.txt");
   for await (const event of watcher) {
     if (event.eventType === "change") fileHandler.emit("change");
   }
