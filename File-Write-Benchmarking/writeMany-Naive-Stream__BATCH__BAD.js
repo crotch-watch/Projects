17c29be4908239724a7a2b0ae6af06bebd8f8d56
crotch-watch <naps_08_bay@icloud.com>
2024-02-03 15:06:21 +0530

    feat: batching with streams(naive)

---
+++ File-Write-Benchmarking/writeMany-Naive-Stream__BATCH__BAD.js
@@ -0,0 +1,20 @@
+const fs = require("node:fs");
+const { Buffer } = require("node:buffer");
+
+const MAX_ITERATIONS = 1_000_000;
+const MAX_BATCH = 10_000;
+
+(async () => {
+  fs.open("text.txt", "w", (error, fd) => {
+    const buffer = Buffer.alloc(MAX_BATCH);
+    const writeStream = fs.createWriteStream("text.txt");
+    console.time("timer");
+    for (let iter = 0; iter < MAX_ITERATIONS; iter++) {
+      const remainder = iter % MAX_BATCH;
+      if (remainder === 0) writeStream.write(buffer);
+      buffer[remainder] = 0x31;
+    }
+    console.timeEnd("timer");
+    fs.close(fd);
+  });
+})();
